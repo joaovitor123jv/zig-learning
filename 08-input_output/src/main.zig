52e1a701ef58de7761c2c7abb97f6e22667ea5e2
João Vitor Antoniassi Segantin <joao.segantin@taomusic.com.br>
2024-12-13 08:28:44 -0300

    Add debug arg parsing from CLI

--- 08-input_output/src/main.zig
+++ 08-input_output/src/main.zig
@@ -1,6 +1,14 @@
 const std = @import("std");
 const os = @import("builtin").os;
 
+const stdout = std.io.getStdOut();
+const stdin = std.io.getStdIn();
+const MAX_STR_RESPONSE = 120;
+
+const FileFormat = enum { Json, Csv, Yaml, Unknown };
+
+const Behavior = struct { input: []u8, output: []u8, inputFormat: FileFormat, outputFormat: FileFormat, configuredByCli: bool };
+
 fn createFile(allocator: std.mem.Allocator, path: []const u8) !void {
     const file = try std.fs.cwd().createFile(
         path,
@@ -18,20 +26,50 @@ fn createFile(allocator: std.mem.Allocator, path: []const u8) !void {
     std.debug.print("Content written on file: {s}\n", .{bytes_read});
 }
 
-pub fn main() !void {
-    const max_file_size = 200;
-    const stdin = std.io.getStdIn();
-    const stdout = std.io.getStdOut();
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+fn askForStrResponse(allocator: std.mem.Allocator, question: []const u8) ![]u8 {
+    try stdout.writeAll(question);
+    try stdout.writeAll("\n-> ");
 
-    try stdout.writeAll("What file do you want to create? ");
+    var result = try stdin.reader().readUntilDelimiterAlloc(allocator, '\n', MAX_STR_RESPONSE);
 
-    var result = try stdin.reader().readUntilDelimiterAlloc(gpa.allocator(), '\n', max_file_size);
+    // Removes \r if is running on windows
     if (os.tag == .windows) {
-        result = std.mem.trimRight(u8, result, '\r'); // Removes \r if running on windows
+        result = std.mem.trimRight(u8, result, '\r');
     }
 
-    try stdout.writer().print("You are creating a file named: {s}\n", .{result});
+    return result;
+}
 
-    try createFile(gpa.allocator(), result);
+fn parseArgs() Behavior {
+    var args = std.process.args();
+    var argsParsed: u8 = 0;
+    _ = args.skip(); // Ignores the first command (the file name for the binary of this software)
+
+    while (args.next()) |result| {
+        argsParsed += 1;
+        std.debug.print("Result = {s}\n", .{result});
+    }
+
+    if (argsParsed > 0) {
+        return Behavior{ .output = "", .input = "", .inputFormat = FileFormat.Unknown, .outputFormat = FileFormat.Unknown, .configuredByCli = true };
+    } else {
+        return Behavior{ .output = "", .input = "", .inputFormat = FileFormat.Unknown, .outputFormat = FileFormat.Unknown, .configuredByCli = true };
+    }
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const behavior = parseArgs();
+
+    if (behavior.configuredByCli) {
+        std.debug.print("Configured by cli", .{});
+    } else {
+        const result = try askForStrResponse(allocator, "What file do you want to create? ");
+
+        try stdout.writer().print("You are creating a file named: {s}\n", .{result});
+
+        try createFile(gpa.allocator(), result);
+    }
 }
